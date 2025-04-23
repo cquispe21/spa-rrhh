73b2c9e91ce5604e6165fdbe3bada2cc7bba14a0
crisdaavid-dev <cquispe@intuitosa.com>
2025-04-23 03:12:27 -0500

    feat: Agregar icono de edición y mejorar la visualización de evaluaciones

--- src/feactures/Evaluacion/Components/EvaluationList.tsx
+++ src/feactures/Evaluacion/Components/EvaluationList.tsx
@@ -13,7 +13,7 @@ import EvaluacionContext, {
 } from "../Context/EvaluacionContext";
 
 import moment from "moment";
-
+import EditIcon from "@/icons/EditIcon";
 
 export default function EvaluacionList() {
   const navigate = useNavigate();
@@ -37,31 +37,43 @@ export default function EvaluacionList() {
       </div>
       <Table>
         <TableHeader>
-          <TableRow>
-            <TableHead>Nombre</TableHead>
-            <TableHead>Descripción</TableHead>
-            <TableHead>Fecha de Creacion</TableHead>
-            <TableHead>Acciones</TableHead>
+          <TableRow >
+            <TableHead className="lg:table-cell md:table-cell hidden">Nombre</TableHead>
+            <TableHead className="lg:table-cell md:table-cell hidden">Descripción</TableHead>
+            <TableHead className="lg:table-cell md:table-cell hidden">Fecha de Creacion</TableHead>
+            <TableHead className="lg:table-cell md:table-cell hidden">Acciones</TableHead>
+            <TableHead className="lg:hidden md:hidden">Informacion</TableHead>
           </TableRow>
         </TableHeader>
         <TableBody>
           {Evaluationlist!.map((evaluation) => (
             <TableRow key={evaluation.idEvaluacion}>
-              <TableHead>{evaluation.title}</TableHead>
-              <TableHead>{evaluation.description}</TableHead>
-              <TableHead>
-
-              {moment(evaluation.fechaCreacion).format("YYYY-MM-DD")}
-
+              <TableHead className="lg:table-cell md:table-cell hidden">{evaluation.title}</TableHead>
+              <TableHead className="lg:table-cell md:table-cell hidden">{evaluation.description}</TableHead>
+              <TableHead className="lg:table-cell md:table-cell hidden">
+                {moment(evaluation.fechaCreacion).format("YYYY-MM-DD")}
               </TableHead>
-              <TableHead>
+              <TableHead className="lg:hidden md:hidden">
+
+                <div className="flex flex-col gap-2">
+                  <h1 className="text-lg font-bold">{evaluation.title}</h1>
+                  <p className="text-sm text-gray-500">{evaluation.description}</p>
+                  <p className="text-sm text-gray-500">
+                    {moment(evaluation.fechaCreacion).format("YYYY-MM-DD")}
+                  </p>
+                </div>
+              </TableHead>
+
+              <TableHead
+              className="flex items-center justify-center"
+              >
                 <Button
-                  text="Ver Detalles"
-                  style="bg-[#374151] hover:bg-[#313a48]"
+                  style="bg-[#374151]  hover:bg-[#313a48]"
                   onClick={async () => {
                     await searchEvaluationId(evaluation.idEvaluacion!);
                     navigate(`/360/evaluations/${evaluation.idEvaluacion}`);
                   }}
+                  icon={<EditIcon />}
                 />
               </TableHead>
             </TableRow>
